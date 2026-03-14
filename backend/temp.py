70da5629cc04f32aee19fd675f43c4c538abeb9e
Your Name <your-email@example.com>
2026-03-14 18:21:52 +0530

    connected to serverless postgres database

--- backend/temp.py
+++
@@ -1,9 +0,0 @@
-from database import SessionLocal
-from models import RecoveryTask
-
-db = SessionLocal()
-
-deleted = db.query(RecoveryTask).delete()
-db.commit()
-
-print("Deleted tasks:", deleted)
